0015fca5ecc60e8053a8ca5ae23cecaecfec7979
Michael Dekker <git@michaeldekker.nl>
2017-02-05 07:57:49 +0100

    Update blockmyaccountfooter.php

--- blockmyaccountfooter.php
+++ blockmyaccountfooter.php
@@ -24,7 +24,7 @@
 *  International Registered Trademark & Property of PrestaShop SA
 */
 
-if (!defined('_PS_VERSION_'))
+if (!defined('_TB_VERSION_'))
 	exit;
 
 class BlockMyAccountFooter extends Module
@@ -33,8 +33,8 @@ class BlockMyAccountFooter extends Module
 	{
 		$this->name = 'blockmyaccountfooter';
 		$this->tab = 'front_office_features';
-		$this->version = '1.6.1';
-		$this->author = 'PrestaShop';
+		$this->version = '2.0.0';
+		$this->author = 'thirty bees';
 		$this->need_instance = 0;
 
 		parent::__construct();
