093f0efd732270fb7c8b760b70830173228bc5e1
dh42 <lesley@dh42.com>
2018-05-15 23:09:45 -0500

    Added build file

---
+++ build.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+CWD_BASENAME=${PWD##*/}
+
+FILES=("logo.gif")
+FILES+=("logo.png")
+FILES+=("style.css")
+FILES+=(".htaccess")
+FILES+=("${CWD_BASENAME}.php")
+FILES+=("${CWD_BASENAME}.tpl")
+FILES+=("Readme.md")
+FILES+=("index.php")
+FILES+=("translations/**")
+
+MODULE_VERSION="$(sed -ne "s/\\\$this->version *= *['\"]\([^'\"]*\)['\"] *;.*/\1/p" ${CWD_BASENAME}.php)"
+MODULE_VERSION=${MODULE_VERSION//[[:space:]]}
+ZIP_FILE="${CWD_BASENAME}/${CWD_BASENAME}-v${MODULE_VERSION}.zip"
+
+echo "Going to zip ${CWD_BASENAME} version ${MODULE_VERSION}"
+
+cd ..
+for E in "${FILES[@]}"; do
+  find ${CWD_BASENAME}/${E}  -type f -exec zip -9 ${ZIP_FILE} {} \;
+done
